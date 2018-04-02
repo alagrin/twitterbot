ef4e464dce77afd8fbcb7f12fef6382f3f5ddf72
Alan Grinberg <alagrin@Alans-MacBook-Air.local>
2018-04-01 21:41:50 -0700

    added config

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const Twit = require('twit');
+const config = require('./config');
 
-
-consumer_key: 'InbDRyFGufLYVYfUamYgxRB9h'
-consumer_secret: 'vcTvYahoMFD957cnbYMrXmG9DmjjQMouOyZ4N5o70opx20T505'
-access_token: '15751425-NUmZd1Z7h4s7EoxK2d58PyZ45DonNC4tb8XzgtY6j'
-access_token_secret: '	cVl5JtPJNlmGJeV8afFIpZBCtdoBWTKU1X6gJfNKPISxu'
-timeout_ms: 60*1000
+//start new session...
+const T = new Twit(config);
 
