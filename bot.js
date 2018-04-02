9e59d431d34c101bb3cd2bb78119304556508575
Alan Grinberg <alagrin@Alans-MacBook-Air.local>
2018-04-01 22:29:09 -0700

    added get/post of tweets

--- bot.js
+++ bot.js
@@ -4,3 +4,17 @@ const config = require('./config');
 //start new session...
 const T = new Twit(config);
 
+//posts tweet to your account
+
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//     console.log(data)
+//   });
+
+//get tweets by searchign text, count, etc.
+T.get('search/tweets', {q: 'Alan Grinberg'}, function(err, data, response) {
+    let tweets = data.statuses;
+    for (let i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+    }
+});
+
